b6a20ccf206c2519039f8eae4041735678fb0bc2
zzhang <zzhang@nullmax.ai>
2021-04-27 21:23:02 +0800

    add

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ git pull
 
 if [[ $(git status -s) ]]
 then
-    echo "The working directory is dirty. Now commit pending change."
+    echo "The working directory is dirty. Now commit pending changes."
     git add . && git commit -m "Publishing to master" && git push origin master -f
 fi
 
